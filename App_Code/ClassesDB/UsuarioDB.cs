4dae18de8bc833ffaaf35fc77588ba0e4fa88714
JonasMoria <moriajonas@gmail.com>
2021-11-11 19:42:23 -0300

    Adicionado pasta App_Code

---
+++ App_Code/ClassesDB/UsuarioDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de UsuarioDB
+/// </summary>
+public class UsuarioDB {
+    public UsuarioDB() {
+
+    }
+}
