728de930668bd98892c03bab234d861cd4fbed8b
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-14 15:22:44 -0300

    Update Mapped.cs

--- App_Code/Mapped.cs
+++ App_Code/Mapped.cs
@@ -1,1 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+
+/// <summary>
+/// Descrição resumida de Mapped
+/// </summary>
+public class Mapped
+{
+
+    public static IDbConnection Connection()
+    {
+
+        MySqlConnection objConexao = new MySqlConnection(ConfigurationManager.AppSettings["StringDeConexaoMySQL"]);
+        objConexao.Open();
+        return objConexao;
+    }
+
+
+    public static IDbCommand Command(string sql, IDbConnection objConexao)
+    {
+        IDbCommand command = objConexao.CreateCommand();
+        command.CommandText = sql;
+        return command;
+    }
+
+
+    public static IDataAdapter Adapter(IDbCommand command)
+    {
+        IDbDataAdapter adap = new MySqlDataAdapter();
+        adap.SelectCommand = command;
+        return adap;
+    }
+
+
+    public static IDataParameter Parameter(string nomeParametro, object valor)
+    {
+        return new MySqlParameter(nomeParametro, valor);
+    }
+
+
+}
+
+
 
