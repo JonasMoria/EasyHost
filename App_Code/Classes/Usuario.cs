f873c056776e9b8d1dae2d177d7e49bbe1f60563
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-14 15:23:29 -0300

    Update Usuario.cs

--- App_Code/Classes/Usuario.cs
+++ App_Code/Classes/Usuario.cs
@@ -1,11 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 
 public class Usuario {
-    public Usuario() {
+  
+    private string _Email;
+    private string _Senha;
+    private string _CPF;
+    private string _NomeEmpresa;
+    
 
+    public string Email
+    {
+        get
+        {
+            return _Email;
+        }
+
+        set
+        {
+            _Email = value;
+        }
+    }
+
+    public string Senha
+    {
+        get
+        {
+            return _Senha;
+        }
+
+        set
+        {
+            _Senha = value;
+        }
+    }
+    public string CPF
+    {
+        get
+        {
+            return _CPF;
+        }
+
+        set
+        {
+            _CPF = value;
+        }
+    }
+         public string NomeEmpresa
+    {
+        get
+        {
+            return _NomeEmpresa;
+        }
+
+        set
+        {
+            _NomeEmpresa = value;
+        }
     }
 }
