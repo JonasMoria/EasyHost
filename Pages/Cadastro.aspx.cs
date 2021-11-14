898ad762b7e12c04b64000c35b0b577745404da9
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-14 15:26:15 -0300

    Update Cadastro.aspx.cs

--- Pages/Cadastro.aspx.cs
+++ Pages/Cadastro.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +6,27 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Pages_Cadastro : System.Web.UI.Page {
+
+    static string url = "http://localhost:61416/";
     protected void Page_Load(object sender, EventArgs e) {
 
     }
+
+    protected void btnCriar_Click(object sender, EventArgs e)
+    {
+        Usuario usu = new Usuario();
+        usu.Email = txtCadUsuario.Text;
+        usu.Senha = txtCadRepetirSenha.Text;
+        usu.CPF = txtDoc.Text;
+        usu.NomeEmpresa = txtCadNomeEmpresa.Text;
+
+       
+
+        UsuarioDB.Insert(usu);
+
+        Response.Redirect(url + "Default.aspx");
+
+
+    }
+
 }
