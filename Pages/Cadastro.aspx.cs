d8ddcc029feeba9dd2ff1acbe641a8a05b3d205d
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-16 19:00:21 -0300

    Alterar Cadastro.aspx.cs

--- Pages/Cadastro.aspx.cs
+++ Pages/Cadastro.aspx.cs
@@ -7,24 +7,45 @@ using System.Web.UI.WebControls;
 
 public partial class Pages_Cadastro : System.Web.UI.Page {
 
-    static string url = "http://localhost:61416/";
+    static string url = "http://localhost:52757/";
     protected void Page_Load(object sender, EventArgs e) {
 
     }
 
     protected void btnCriar_Click(object sender, EventArgs e)
     {
+
+        Funcionario fun = new Funcionario();
+        fun.Nome = "0";
+        fun.CPF = "1";
+
+        Administrador adm = new Administrador();
+        adm.CPF = txtDoc.Text;
+        adm.Nome = "teste";
+
+        
+
         Usuario usu = new Usuario();
         usu.Email = txtCadUsuario.Text;
         usu.Senha = txtCadRepetirSenha.Text;
-        usu.CPF = txtDoc.Text;
-        usu.NomeEmpresa = txtCadNomeEmpresa.Text;
+        usu.ADM_CPF = txtDoc.Text;
+        usu.FUN_CPF = "0";
+        usu.NomeEmpresa = txtCadNomeEmpresa.Text;  
 
-       
+       if (usu.ADM_CPF != "")
+        {
+            FuncionarioDB.Insert(fun);
+            AdministradorDB.Insert(adm);
+            UsuarioDB.Insert(usu);
 
-        UsuarioDB.Insert(usu);
-
-        Response.Redirect(url + "Default.aspx");
+            Response.Redirect(url + "Default.aspx");
+        }
+          
+            
+        
+        
+        
+      
 
 
     }
