56e14e24c914a3cad87bfc5b499cc1d32fcd4e1a
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-19 20:15:48 -0300

    Update Hospedes.aspx.cs

--- Pages/Hospedes.aspx.cs
+++ Pages/Hospedes.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,5 +8,24 @@ using System.Web.UI.WebControls;
 public partial class Pages_Hospedes : System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
 
+    }
+
+    protected void btn_CadastrarHos_Click(object sender, EventArgs e)
+    {
+        Hospede hos = new Hospede();
+        hos.Nome = txtNomeHospede.Text;
+        hos.Telefone = txtCelHospede.Text;
+        hos.AdmCPF = "1234567";
+
+        if (AdministradorDB.Cadastra_Hospede(hos).Equals("Sucesso"))
+        {
+            lblTexto.Text = "Hóspede cadastrado com sucesso!";
+            lblTexto.Visible = true;
+        }
+        else
+        {
+            lblTexto.Text = "Erro no cadastro. Verifique os dados.";
+            lblTexto.Visible = true;
+        }
     }
 }
