983562f4b77452ef510406bb76ac93f8de811f2e
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-19 20:17:04 -0300

    Update Quartos.aspx.cs

--- Pages/Quartos.aspx.cs
+++ Pages/Quartos.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +7,27 @@ using System.Web.UI.WebControls;
 
 public partial class Pages_Quartos : System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
+
+    }
+
+    protected void btn_CadastrarQua_Click(object sender, EventArgs e)
+    {
+        Quarto qua = new Quarto();
+
+        qua.Nome = txtNomeQuarto.Text;
+        qua.Situacao = "Disponivel";
+        qua.AdmCPF = "1234567";
+
+        if (AdministradorDB.Cadastra_Quarto(qua).Equals("Sucesso"))
+        {
+            lblTexto.Text = "Quarto cadastrado com sucesso!";
+            lblTexto.Visible = true;
+        }
+        else
+        {
+            lblTexto.Text = "Erro no cadastro. Verifique os dados.";
+            lblTexto.Visible = true;
+        }
 
     }
 }
