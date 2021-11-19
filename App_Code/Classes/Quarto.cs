bb6a94516b3a4a6269b703ff5fa519b572f32861
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-19 20:07:42 -0300

    Create Quarto.cs

---
+++ App_Code/Classes/Quarto.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Quarto
+/// </summary>
+public class Quarto
+{
+
+    private string _Nome;
+    private string _Situacao;
+    private string _AdmCPF;
+    
+    public Quarto()
+    {
+        //
+        // TODO: Adicionar lógica do construtor aqui
+        //
+    }
+
+    public string Nome
+    {
+        get
+        {
+            return _Nome;
+        }
+
+        set
+        {
+            _Nome = value;
+        }
+    }
+
+    public string Situacao
+    {
+        get
+        {
+            return _Situacao;
+        }
+
+        set
+        {
+            _Situacao = value;
+        }
+    }
+
+    public string AdmCPF
+    {
+        get
+        {
+            return _AdmCPF;
+        }
+
+        set
+        {
+            _AdmCPF = value;
+        }
+    }
+
+
+
+
+}
