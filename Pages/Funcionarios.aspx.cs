b1cdef55e1d90920523a2c3342b1a72a86f77d6a
Mayara Castro <46913494+mambc@users.noreply.github.com>
2021-11-19 20:12:56 -0300

    Update Funcionarios.aspx.cs

--- Pages/Funcionarios.aspx.cs
+++ Pages/Funcionarios.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +6,33 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Pages_Funcionarios : System.Web.UI.Page {
+
+    static string url = "http://localhost:52757/";
     protected void Page_Load(object sender, EventArgs e) {
+
+    }
+
+    protected void btn_CadasdastrarFun_Click(object sender, EventArgs e)
+    {
+        
+        Funcionario fun = new Funcionario();
+
+        fun.CPF = txtCPFFuncionario.Text;
+        fun.Nome = txtCadFuncionario.Text;
+        fun.Email = txtEmailFuncionario.Text;
+        fun.Senha = txtRepSenhaFuncionario.Text;
+        fun.AdmCPF = "1234567";
+
+        if (AdministradorDB.Cadastra_Funcionario(fun).Equals("Sucesso"))
+        {
+            lblTexto.Text = "Funcionário cadastrado com sucesso!";
+            lblTexto.Visible = true;
+        }
+        else
+        {
+            lblTexto.Text = "Erro no cadastro. Verifique os dados.";
+            lblTexto.Visible = true;
+        }
 
     }
 }
